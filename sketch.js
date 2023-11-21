582d240368139ac29a61ebcea24c39d03732964e
LllimaBeans <146811819+LllimaBeans@users.noreply.github.com>
2023-11-21 12:16:56 -0600

    Started working on menu bar for diff screens

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-// Project Title
-// Your Name
+// Shrimp
+// Laura Lima
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let whichScreen = "startScreen";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,4 +14,36 @@ function setup() {
 function draw() {
   background(220);
   circle(mouseX, mouseY, 100);
+}
+
+function changeScreen() {
+  if (whichScreen === "startScreen"){
+    // instruction page here
+  }
+  else if (whichScreen === "farm") {
+    // farm here
+  }
+  else if (whichScreen === "shopSelling") {
+    // customers here
+  }
+}
+
+class CurrentScreen {
+  constructor() {
+
+  }
+
+  display() {
+    
+  }
+}
+
+class Crustacean {
+  constructor() {
+
+  }
+
+  display() {
+
+  }
 }
