a407d498e9c2f86cf41fca527ed5934a16056159
LllimaBeans <146811819+LllimaBeans@users.noreply.github.com>
2023-11-22 12:17:29 -0600

    I have made buttons that almost work

--- sketch.js
+++ sketch.js
@@ -5,38 +5,56 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let whichScreen = "startScreen";
+let whichScreen = "start";
+let buttonRule, buttonFarm, buttonShop;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0, 100, 0);
 }
 
-function draw() {
-  background(220);
-  circle(mouseX, mouseY, 100);
+function buttonCalls() {
+  buttonRule = createButton("Rules");
+  buttonRule.position(100, height - 60);
+  buttonRule.size(60, 30);
+  buttonRule.mousePressed(whichScreen = "start");
+
+
+  buttonFarm = createButton("Farm");
+  buttonFarm.position(200, height - 60);
+  buttonFarm.size(60, 30);
+  buttonFarm.mousePressed(whichScreen = "farm");
+
+  buttonShop = createButton("Shop");
+  buttonShop.position(300, height - 60);
+  buttonShop.size(60, 30);
+  buttonShop.mousePressed(TempPleaseForgiveMe);
+}
+
+function TempPleaseForgiveMe(){
+  whichScreen = "shop";
 }
 
 function changeScreen() {
-  if (whichScreen === "startScreen"){
-    // instruction page here
+  if (whichScreen === "start"){
+    // display rules/homescreen here
+    background(100, 0, 0);
   }
-  else if (whichScreen === "farm") {
-    // farm here
+  else if (whichScreen === "farm"){
+    // display farm here
+    background(0, 0, 100);
   }
-  else if (whichScreen === "shopSelling") {
-    // customers here
-  }
-}
-
-class CurrentScreen {
-  constructor() {
-
-  }
-
-  display() {
-    
+  else if (whichScreen === "shop") {
+    // displau shop here
+    background(255);
   }
 }
+
+function draw() {
+  changeScreen();
+  buttonCalls();
+}
+
 
 class Crustacean {
   constructor() {
