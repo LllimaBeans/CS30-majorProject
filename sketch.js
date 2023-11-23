e41768a63abec61bd00344e67aa84a894d92cff8
LllimaBeans <146811819+LllimaBeans@users.noreply.github.com>
2023-11-23 11:15:21 -0600

    Buttons now work 100% of the time

--- sketch.js
+++ sketch.js
@@ -6,55 +6,51 @@
 // - describe what you did to take this project "above and beyond"
 
 let whichScreen = "start";
-let buttonRule, buttonFarm, buttonShop;
+
+let buttonX = 200;
+let buttonY = 200;
+let buttonW = 50;
+let buttonH = 30;
+let buttonSpace = 100;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0, 100, 0);
-}
-
-function buttonCalls() {
-  buttonRule = createButton("Rules");
-  buttonRule.position(100, height - 60);
-  buttonRule.size(60, 30);
-  buttonRule.mousePressed(whichScreen = "start");
-
-
-  buttonFarm = createButton("Farm");
-  buttonFarm.position(200, height - 60);
-  buttonFarm.size(60, 30);
-  buttonFarm.mousePressed(whichScreen = "farm");
-
-  buttonShop = createButton("Shop");
-  buttonShop.position(300, height - 60);
-  buttonShop.size(60, 30);
-  buttonShop.mousePressed(TempPleaseForgiveMe);
-}
-
-function TempPleaseForgiveMe(){
-  whichScreen = "shop";
-}
-
-function changeScreen() {
-  if (whichScreen === "start"){
-    // display rules/homescreen here
-    background(100, 0, 0);
-  }
-  else if (whichScreen === "farm"){
-    // display farm here
-    background(0, 0, 100);
-  }
-  else if (whichScreen === "shop") {
-    // displau shop here
-    background(255);
-  }
 }
 
 function draw() {
-  changeScreen();
-  buttonCalls();
+  start();
+  shop();
+  farm();
 }
 
+function mousePressed(){
+  // Pressing shop
+  if (mouseX > buttonX && mouseX < buttonX + buttonW && mouseY > buttonY && mouseY < buttonY + buttonH){
+    background(random(255));
+  }
+  // Pressing farm
+  if (mouseX > buttonX + buttonSpace && mouseX < buttonX + buttonSpace + buttonW && mouseY > buttonY && mouseY < buttonY + buttonH){
+    background(random(255));
+  }
+  // Pressing start
+  if (mouseX > buttonX + buttonSpace * 2 && mouseX < buttonX + buttonSpace * 2 + buttonW && mouseY > buttonY && mouseY < buttonY + buttonH){
+    background(random(255));
+  }
+}
+
+function shop() {
+  let button1 = rect(buttonX, buttonY, buttonW, buttonH);
+}
+
+function farm() {
+  let button2 = rect(buttonX + buttonSpace, buttonY, buttonW, buttonH);
+}
+
+function start() {
+  let button3 = rect(buttonX + buttonSpace * 2, buttonY, buttonW, buttonH);
+}
 
 class Crustacean {
   constructor() {
